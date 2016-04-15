2fcc88f6bc3ec250cba2a559e9718f89dbb22de8
uveavanto <uve@Yuveshs-MacBook-Air.local>
2016-04-15 16:02:57 +0100

    started making functions for later, made a basic map layout using a matrix

--- textadventure/sketch.js
+++ textadventure/sketch.js
@@ -1,7 +1,30 @@
 function setup() {
+  createCanvas(windowWidth,windowHeight);
   
+  var mapMatrix = 
+  (
+  	(0,0,2,0,0),
+  	(0,0,3,3,0),
+  	(0,0,0,3,0),
+  	(0,0,3,3,0),
+  	(0,0,1,0,0)
+  );
+  //stores the map of the game
+  //0 = wall
+  //1 = player start
+  //2 = end
+  //3 = empty room
 }
 
 function draw() {
-  
+  drawBackground();
+  //drawInventory();
+  //drawHP();
+  //drawInventory();
+  //drawText();
+}
+
+function drawBackground()
+{
+	background(20,70,140);
 }
